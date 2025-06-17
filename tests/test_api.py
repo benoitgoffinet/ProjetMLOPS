96c5331f6ce8630cbd5ccf9e2079b6483ffb4680
benoitgoffinet <benoitgoffinet@live.fr>
2025-06-17 09:03:09 +0200

    Correction test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,6 @@
 import requests
 
 def test_predict_endpoint():
-    response = requests.post("http://localhost:8000/predict", json={"question": "How to use pandas?"})
+    response = requests.post("http://localhost:8000/predict", data="How to use pandas?"))
     assert response.status_code == 200
     assert "tags" in response.json()
